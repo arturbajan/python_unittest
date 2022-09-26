b34ffda478afb733421846b94cd6c0042171516f
artur.bajan <artur.bajan@gmail.com>
2022-09-26 23:05:36 +0200

    2 z 3 zadań

---
+++ utest.py
@@ -0,0 +1,17 @@
+import unittest
+import zadanie
+
+class TestMainMethod(unittest.TestCase):
+    
+    def test_dodaj(self):
+        s=zadanie.dodaj(2,2,2)
+        self.assertEqual(s,6)
+
+
+    def test_wpis_imie(self):
+        im=zadanie.wpisz_imie('Artur')
+        self.assertEqual(im,'Dzien dobry ARTUR')
+        
+  #  def test_pole_kola(self):
+    #    self.assertEqual(pole_kola(),12)
+        
---
+++ zadanie.py
@@ -0,0 +1,9 @@
+def dodaj(a,b,c):
+    return a+b+c
+def wpisz_imie(imie):
+   imie=str(input('Podaj Imie\n'))
+   return f"Dzien dobry {imie.upper()}"
+#def pole_kola(self):
+    #self.r=float(input("Podaj promien"))
+  #  return 3.14*pow(2,2)
+
